47c774b59606a7e67159d915ed9c1cec90b78e65
Lucas Astor <lucasastor@gmail.com>
2019-08-20 23:35:20 -0300

    pd1 ut2

--- UT2/PD1/src/ArbolGenerico.java
+++ UT2/PD1/src/ArbolGenerico.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package UT2.ej1;
-
-/**
- *
- * @author Diego
- */
 public class ArbolGenerico  implements IArbolGenerico{
     
     private INodoGenerico raiz;
@@ -78,6 +67,6 @@ public class ArbolGenerico  implements IArbolGenerico{
     @Override
     public void listarIndentado() {
         if (this.raiz != null){raiz.listarIndentado("");}
-        else{System.out.println("Arbol vacío.");}
+        else{System.out.println("Arbol vacÃ­o.");}
     }
 }
