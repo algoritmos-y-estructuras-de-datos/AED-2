2589c7b06d2d28261e3f82e527d3fda057445117
Lucas Astor <lucasastor@gmail.com>
2019-08-26 19:54:12 -0300

    tries

---
+++ UT2/TA2/src/TArbolTrie.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lucas
+ */
+public class TArbolTrie {
+    private TNodoTrie raiz;
+    public void insertar(String palabra){
+        if(raiz == null){
+            raiz = new TNodoTrie();
+        }
+        raiz.insertar(palabra);
+    }
+    public void imprimir() {
+        if(raiz != null){
+            raiz.imprimir();
+        }
+    }
+    public Integer buscar(String palabra){
+        return raiz.buscar(palabra, 0);
+    }
+}
