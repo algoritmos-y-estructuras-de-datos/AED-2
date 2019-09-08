d1a6f0256149e349708e0c50162e8c4632268a2b
Lucas Astor <lucasastor@gmail.com>
2019-09-08 17:23:59 -0300

    Finalizo Ej1

--- UT3/PD3/EJ1/src/Main.java
+++ UT3/PD3/EJ1/src/Main.java
@@ -1,5 +1,6 @@
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /*
@@ -7,7 +8,6 @@ import java.util.Map;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author lucas
@@ -18,14 +18,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Map mapa = new HashMap();
+        Map< String, Object> mapa = new HashMap<>();
         // Inserto las claves
         mapa.put("a", null);
         mapa.put("b", null);
         mapa.put("c", null);
         mapa.put("d", null);
         mapa.put("e", null);
+        // Imprimo tamaño antes de eliminar
+        System.out.println(mapa.size());
         // Elimino todas las claves
+        for (Iterator<String> iterator = mapa.keySet().iterator(); iterator.hasNext();) {
+            String key = iterator.next();
+            if (!key.equals("f")) {
+                iterator.remove();
+            }
+        }
+        // Imprimo tamaño luego de eliminar
+        System.out.println(mapa.size());
+
     }
-    
+
 }
