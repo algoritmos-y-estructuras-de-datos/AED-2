ef65ef3fdc1d72de3698019b7601c31bb2ef01f2
Lucas Astor <lucasastor@gmail.com>
2019-09-08 16:54:07 -0300

    Creo project para PD3 - EJ1

---
+++ UT3/PD3/EJ1/src/Main.java
@@ -0,0 +1,31 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lucas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Map mapa = new HashMap();
+        // Inserto las claves
+        mapa.put("a", null);
+        mapa.put("b", null);
+        mapa.put("c", null);
+        mapa.put("d", null);
+        mapa.put("e", null);
+        // Elimino todas las claves
+    }
+    
+}
