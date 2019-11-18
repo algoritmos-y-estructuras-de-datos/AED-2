8de1915df2c2a1a9a80e7814902eb22cefaf6d08
'astorlucas' <'lucasastor@gmail.com'>
2019-11-18 13:19:22 -0300

    Finalizo preparacion

--- UT7/BinSort/src/Main.java
+++ UT7/BinSort/src/Main.java
@@ -1,9 +1,12 @@
+
+import java.util.LinkedList;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author lucas
@@ -14,7 +17,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
+    public static void binsort(int[] A) {
+        List[] B = new LinkedList[9 + 1];
+        Object item;
+        for (int i = 0; i <= 9; i++) {
+            B[i] = new LinkedList();
+        }
+        for (int i = 0; i < A.length; i++) {
+            B[A[i]].add(new Integer(A[i]));
+        }
+        int pos = 0;
+        for (int i = 0; i <= 9; i++) {
+            for (B[i].moveToStart; (item = B[i].getValue()) != null; B[i].next()) {
+                A[pos++] = (Integer) item;
+            }
+        }
+    }
+}
+
 }
