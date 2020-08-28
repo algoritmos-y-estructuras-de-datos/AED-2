f42f5467e29c758a0926af935e1cfda0349acce1
astorlucas <lucasastor@gmail.com>
2020-08-27 23:38:09 -0300

    TArbol y TNodo actualizados

--- UT2_PD3/src/TArbolTrie.java
+++ UT2_PD3/src/TArbolTrie.java
@@ -9,29 +9,47 @@
  * @author lucas
  */
 public class TArbolTrie {
+
     public TNodoTrie raiz;
-    
-    public void insertar(String palabra){
-        if(raiz == null){
+
+    public void insertar(String palabra) {
+        if (raiz == null) {
             raiz = new TNodoTrie();
         }
         raiz.insertar(palabra);
     }
-    
-    public int insertarConPaginas(String palabra,Integer paginas){
-        if(raiz == null){
+
+    public int insertarConPaginas(String palabra, Integer paginas) {
+        if (raiz == null) {
             raiz = new TNodoTrie();
         }
-        return raiz.insertarConPaginas(palabra,paginas);
+        return raiz.insertarConPaginas(palabra, paginas);
     }
-    
+
     public void imprimir() {
-        if(raiz != null){
+        if (raiz != null) {
             raiz.imprimir();
         }
     }
-    public Integer buscar(String palabra){
+
+    public Integer buscar(String palabra) {
         return raiz.busqueda(palabra);
     }
+
+    public Integer altura(TNodoTrie nodo) {
+        if (raiz == null) {
+            return 0;
+        } else {
+            return raiz.altura(nodo);
+        }
+    }
     
+     public Integer tamanio(TNodoTrie nodo) {
+        if (raiz == null) {
+            return 0;
+        } else {
+            return raiz.tamanio(nodo);
+        }
+    }
+
 }
