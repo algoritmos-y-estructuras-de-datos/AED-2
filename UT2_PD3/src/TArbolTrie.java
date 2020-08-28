146719a8ee56bdf5d44481a77e7d9e6f585090b8
astorlucas <lucasastor@gmail.com>
2020-08-27 23:18:05 -0300

    tries

---
+++ UT2_PD3/src/TArbolTrie.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lucas
+ */
+public class TArbolTrie {
+    public TNodoTrie raiz;
+    
+    public void insertar(String palabra){
+        if(raiz == null){
+            raiz = new TNodoTrie();
+        }
+        raiz.insertar(palabra);
+    }
+    
+    public int insertarConPaginas(String palabra,Integer paginas){
+        if(raiz == null){
+            raiz = new TNodoTrie();
+        }
+        return raiz.insertarConPaginas(palabra,paginas);
+    }
+    
+    public void imprimir() {
+        if(raiz != null){
+            raiz.imprimir();
+        }
+    }
+    public Integer buscar(String palabra){
+        return raiz.busqueda(palabra);
+    }
+    
+}
---
+++ UT2_PD3/src/TNodoTrie.java
@@ -0,0 +1,119 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lucas
+ */
+public class TNodoTrie {
+
+    private static final int CANT_CHR_ABECEDARIO = 145;
+    public TNodoTrie[] hijos;
+    private boolean esPalabra;
+    private ArrayList<Integer> paginas = new ArrayList<Integer>();
+
+    public TNodoTrie() {
+        hijos = new TNodoTrie[CANT_CHR_ABECEDARIO];
+        esPalabra = false;
+    }
+
+    public int insertarConPaginas(String palabras, Integer paginas) {
+        TNodoTrie nodo = this;
+        int comparaciones = 0;
+        for (int i = 0; i < palabras.length(); i++) {
+            int indice = palabras.charAt(i) - 'a';
+            if (nodo.hijos[indice] == null) {
+                nodo.hijos[indice] = new TNodoTrie();
+            }
+            comparaciones++;
+            nodo = nodo.hijos[indice];
+        }
+        nodo.esPalabra = true;
+        nodo.paginas.add(paginas);
+        return comparaciones;
+    }
+
+    public String paginasDePalabra(String palabras) {
+        TNodoTrie nodo = this;
+        for (int i = 0; i < palabras.length(); i++) {
+            int indice = palabras.charAt(i) - 'a';
+            if (nodo.hijos[indice] == null) {
+                return "No se encuentra";
+            }
+            nodo = nodo.hijos[indice];
+        }
+        if (nodo.esPalabra) {
+            return nodo.paginas.toString();
+        } else {
+            return "No se encuentra";
+        }
+    }
+
+    public void insertar(String unaPalabra) {
+        TNodoTrie nodo = this;
+        int comparaciones = 0;
+        for (int c = 0; c < unaPalabra.length(); c++) {
+            int indice = unaPalabra.charAt(c) - 'a';
+            if (nodo.hijos[indice] == null) {
+                nodo.hijos[indice] = new TNodoTrie();
+            }
+            comparaciones++;
+            nodo = nodo.hijos[indice];
+        }
+        nodo.esPalabra = true;
+        System.out.println(comparaciones);
+    }
+
+    public int busqueda(String unaPalabra) {
+        TNodoTrie nodo = this;
+        int comparaciones = 0;
+        for (int c = 0; c < unaPalabra.length(); c++) {
+            int indice = unaPalabra.charAt(c) - 'a';
+            if (nodo.hijos[indice] == null) {
+                return 0;
+            } else {
+                comparaciones++;
+                nodo = nodo.hijos[indice];
+            }
+        }
+        if (nodo.esPalabra) {
+            return comparaciones;
+        } else {
+            return 0;
+        }
+    }
+
+    private void imprimir(String s, TNodoTrie nodo) {
+        if (nodo != null) {
+            if (nodo.esPalabra) {
+                System.out.println(s + ", " + nodo.paginas.toString());
+            }
+            for (int c = 0; c < CANT_CHR_ABECEDARIO; c++) {
+                if (nodo.hijos[c] != null) {
+                    imprimir(s + (char) (c + 'a'), nodo.hijos[c]);
+                }
+            }
+        }
+    }
+
+    public void imprimir() {
+        imprimir("", this);
+    }
+
+    public int altura(TNodoTrie node) {
+        int lev = 1;
+        if (node != null) {
+            TNodoTrie current = node;
+            //int lev = 1;
+            for (TNodoTrie child : node.hijos) {
+                lev = Math.max(lev, 1 + altura(child));
+            }
+        }
+        return lev;
+    }
+}
