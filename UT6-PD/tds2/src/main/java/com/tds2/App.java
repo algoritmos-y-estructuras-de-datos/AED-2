8392a7f0a6133ffbc899f84740a1231c65b25fa9
astorlucas <lucasastor@gmail.com>
2021-08-15 20:54:18 -0300

    un proyecto maven, de prueba

---
+++ UT6-PD/tds2/src/main/java/com/tds2/App.java
@@ -0,0 +1,13 @@
+package com.tds2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
