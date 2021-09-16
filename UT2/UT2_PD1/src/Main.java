91fbb973bf1ffad49311f64d1e6b76f32be9b553
astorlucas <lucasastor@gmail.com>
2021-09-15 23:51:55 -0300

    no hice nada solo leí

--- UT2/UT2_PD1/src/Main.java
+++ UT2/UT2_PD1/src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         ArbolGenerico arbol = new ArbolGenerico();
+        
         arbol.insertar("RECTORIA", "");
         arbol.insertar("VICERRECTORIA ADMINISTRATIVA", "RECTORIA");
         arbol.insertar("VICERRECTORIA ACADEMICA", "RECTORIA");
@@ -12,6 +13,8 @@ public class Main {
         arbol.insertar("FACULTAD DE CIENCIAS EMPRESARIALES", "VICERRECTORIA ACADEMICA");
         arbol.insertar("DEPARTAMENTO DE MATEMTICAS","FACULTAD DE INGENIERIA Y TECNOLOGIAS");
         arbol.insertar("DEPARTAMENTO DE INFORMATICA Y CIENCIAS DE LA COMPUTACIÓN","FACULTAD DE INGENIERIA Y TECNOLOGIAS");
+        arbol.insertar("NUEVA RECTORIA","");
+        System.out.println(arbol.insertar("NUEVA RECTORIA3","pepe"));
         arbol.listarIndentado();
     }
     
