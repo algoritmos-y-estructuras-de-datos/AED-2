89020481d56eae7f3992f9a2b37b55af600ed462
astorlucas <lucasastor@gmail.com>
2021-09-26 00:33:25 -0300

    ut2 pd4

--- UT2/pd4/src/main/java/ut2/pd/App.java
+++ UT2/pd4/src/main/java/ut2/pd/App.java
@@ -1,4 +1,4 @@
-package ut1.pd;
+package ut2.pd;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        System.out.println( "Hello World!" );
     }
 }
