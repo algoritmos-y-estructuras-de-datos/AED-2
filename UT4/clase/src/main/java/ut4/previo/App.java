fdfdbc045abf3f77f5e61419fae5c7e87aaf86f2
astorlucas <lucasastor@gmail.com>
2021-10-24 22:15:48 -0300

    primera parte ut4

---
+++ UT4/clase/src/main/java/ut4/previo/App.java
@@ -0,0 +1,13 @@
+package ut4.previo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
