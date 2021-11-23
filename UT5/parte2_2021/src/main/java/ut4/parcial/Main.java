2657cbf25f68a1bef601b28cbe04c4b7859ecd76
astorlucas <lucasastor@gmail.com>
2021-11-22 23:33:28 -0300

    obra maestra copiada

--- UT5/parte2_2021/src/main/java/ut4/parcial/Main.java
+++ UT5/parte2_2021/src/main/java/ut4/parcial/Main.java
@@ -7,10 +7,10 @@ public class Main {
         TGrafoNoDirigido grafitoNoDirigidito = (TGrafoNoDirigido) UtilGrafos.cargarGrafo("UT5/parte2_2021/src/main/java/ut4/parcial/personas.txt", "UT5/parte2_2021/src/main/java/ut4/parcial/amistades.txt",
                 false, TGrafoNoDirigido.class);
 
+        
+        //Indicar todos los contactos alcanzables con no más de una cantidad determinada de enlaces (por 
+        //ejemplo, no más de 6 enlaces, o sea, no más de 5 intermediarios).
         Collection<TVertice> conexiones = grafitoNoDirigidito.listarContactos("Fabian", 2);
-
-        // Double[][] matrizpd1 = UtilGrafos.obtenerMatrizCostos(grafitoNoDirigidito.getVertices());
-        // UtilGrafos.imprimirMatrizMejorado(matrizpd1, grafitoNoDirigidito.getVertices(), "Matriz chequiada");
 
         for(TVertice c : conexiones){
             System.out.println(c.getEtiqueta());
