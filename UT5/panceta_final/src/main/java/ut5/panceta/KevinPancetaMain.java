3f2de3c00df3bb2e9660596fa0a57bc3354fbe1d
astorlucas <lucasastor@gmail.com>
2021-11-25 20:51:07 -0300

    Co-authored-by: Mauro Botti <MauroBW@users.noreply.github.com> Co-authored-by: Facundo Badano <facundobadano6195@gmail.com>

--- UT5/panceta_final/src/main/java/ut5/panceta/KevinPancetaMain.java
+++ UT5/panceta_final/src/main/java/ut5/panceta/KevinPancetaMain.java
@@ -7,17 +7,17 @@ public class KevinPancetaMain {
     
     public static void main(String[] args) {
         
-        TGrafoNoDirigido grafoKevinBacon = (TGrafoNoDirigido) UtilGrafos.cargarGrafo("UT5/panceta/src/main/java/ut5/kevin/actores.csv", "UT5/panceta/src/main/java/ut5/kevin/en_pelicula.csv",
+        TGrafoNoDirigido grafoKevinBacon = (TGrafoNoDirigido) UtilGrafos.cargarGrafo("UT5/panceta_final/src/main/java/ut5/panceta/actores.csv", "UT5/panceta_final/src/main/java/ut5/panceta/en_pelicula.csv",
                 false, TGrafoNoDirigido.class);
 
         //System.out.println(grafoKevinBacon.numBacon("Kevin_Bacon")); 
         
-        String[] vertices = ManejadorArchivosGenerico.leerArchivo("UT5/panceta/src/main/java/ut5/kevin/actores.csv",true);
+        String[] vertices = ManejadorArchivosGenerico.leerArchivo("UT5/panceta_final/src/main/java/ut5/panceta/actores.csv",true);
         List<TVertice> verticesList = new ArrayList<TVertice>();
         for (String verticeString : vertices) {
             if ((verticeString != null) && (! verticeString.trim().equals(""))) {
                 verticeString = verticeString.split(",")[0];
-                System.out.println(grafoKevinBacon.numBacon(verticeString));
+                System.out.println(verticeString + " : " + grafoKevinBacon.numBacon(verticeString));
             }
         }
         
