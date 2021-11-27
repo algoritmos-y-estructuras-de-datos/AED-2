0afeacd214ceb56646cc6b4641e33c47fda1f8fd
Lucas Astor <lucasastor@gmail.com>
2021-11-26 23:35:20 -0300

    anillos contagio primer commit

---
+++ UT5/parte2_2020/src/main/java/ut5/parcial2/AnillosContagio.java
@@ -0,0 +1,15 @@
+package ut5.parcial2;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class AnillosContagio<E,F> extends HashMap<E,F>{
+
+    public Map<Integer,Persona> listaContactos = new HashMap<>();
+
+    public void agregarContagio(int distanciaContacto, Persona laPersona){
+            
+    }
+}
